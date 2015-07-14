49dbe425f5aa2f74ebd88588c6834ece129daec8
Torben Seebach <yeah@seebach.dk>
2015-07-14 23:51:48 +0200

    new release

--- Parallax.js
+++ Parallax.js
@@ -36,7 +36,7 @@ require( ["js/qlik"], function ( qlik ) {
 	app.getObject('QV-Services-03','MdpsUpA');
 
 	app.getObject('QV-portofolio-01','nRxXG', { noInteraction:true });
-	app.getObject('QV-Modal-01','nRxXG');
+/*	app.getObject('QV-Modal-01','nRxXG'); */
 	app.getObject('QV-portofolio-02','pDKRhr', { noInteraction:true });
 	app.getObject('QV-Modal-02','pDKRhr');
 	app.getObject('QV-portofolio-03','JEBdZz', { noInteraction:true });
@@ -53,11 +53,39 @@ require( ["js/qlik"], function ( qlik ) {
 	app.getObject('QV-time-03','ARNmpdM');
 	app.getObject('QV-time-04','ARNmpdM');
 
+	app.getObject('QV-Map','VcKjw');
+
+
 	//create cubes and lists -- inserted here --
 	/* $('#portfolioModal1').on('show.bs.modal', function (event) {
 	  console.log('modelled');
 	  console.log(app);
 	  app.getObject('QV-Modal-01','nRxXG');
 	}) */
-
+	$(document).ready( function() {
+	    $("#modal-1").on("click", function() {
+	//			var app = qlik.openApp('e59234ce-6a8d-4ab4-b266-c1b46e6179c3', config);
+				console.log('click #modal-1');
+	      app.getObject('QV-Modal-01','nRxXG');
+	    });
+	});
 } );
+
+
+
+$(document).ready(function (e) {
+
+$('.QV-Modal-01').mousemove(function (e) {
+id = $(this).attr('id');
+$('.selections-data-area-ui canvas').css('position', 'fixed');
+$('.selections-data-area-ui canvas').css('top', y+40);
+// $('.qv-selection-toolbar').css('z-index', 0);
+});
+/*
+$('body').scroll(function() {
+y = $('#'+id).offset().top;
+$('.qv-selection-toolbar').css('top', y-40);
+$('.qv-selection-toolbar').css('z-index', 0);
+});
+*/
+});
